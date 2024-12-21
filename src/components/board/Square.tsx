3912ba21bb695a2681f53f38a82bb2017ccc05f2
Nikola Kenjic <nikola.nico89@gmail.com>
2024-12-21 18:22:24 +0100

    icon shadow

--- src/components/board/Square.tsx
+++ src/components/board/Square.tsx
@@ -1,9 +1,11 @@
+import { PlayerColor } from '../../models/PlayerModel';
 import SquareModel from '../../models/SquareModel';
 import { getPieceIcon } from '../../piece-service';
 import {
   getSquareCoordinates,
   isLightSquare,
 } from '../../services/square-service';
+import './square.css';
 
 interface Props {
   square: SquareModel;
@@ -18,7 +20,13 @@ const Square = ({ square }: Props) => {
     >
       <span>{getSquareCoordinates(square)}</span>
       {square.piece && (
-        <i className={`fa-solid ${getPieceIcon(square.piece)}`}></i>
+        <i
+          className={`fa-solid ${getPieceIcon(square.piece)} ${
+            square.piece.color === PlayerColor.WHITE
+              ? 'text-white icon-shadow-black'
+              : 'text-black icon-shadow-white'
+          }`}
+        ></i>
       )}
     </div>
   );
